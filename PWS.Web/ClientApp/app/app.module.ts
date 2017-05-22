ec58726e6393b9c60d67e928cc52cbba538c5fe5
Petar Petrov <primas.petrov@gmail.com>
2017-05-22 16:22:15 +0300

    Extracted blog component

--- PWS.Web/ClientApp/app/app.module.ts
+++ PWS.Web/ClientApp/app/app.module.ts
@@ -2,23 +2,23 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { UniversalModule } from 'angular2-universal';
 
-import { AppComponent } from './components/app/app.component'
-
+import { AppComponent } from './components/app/app.component';
 import { HomeComponent } from './components/home/home.component';
+import { BlogComponent } from './components/blog/blog.component';
 
 @NgModule({
     bootstrap: [ AppComponent ],
     declarations: [
-        AppComponent,
-        
-        HomeComponent
+        AppComponent,        
+        HomeComponent,
+        BlogComponent
     ],
     imports: [
         UniversalModule, // Must be first import. This automatically imports BrowserModule, HttpModule, and JsonpModule too.
         RouterModule.forRoot([
             { path: '', redirectTo: 'home', pathMatch: 'full' },
             { path: 'home', component: HomeComponent },
-
+            { path: 'blog', component: BlogComponent },
             { path: '**', redirectTo: 'home' }
         ])
     ]
--- PWS.Web/ClientApp/app/components/home/home.component.ts
+++ PWS.Web/ClientApp/app/components/home/home.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 
+import { BlogComponent } from '../blog/blog.component'
+
 @Component({
     selector: 'home',
     templateUrl: './home.component.html',
     styleUrls: ['./home.component.css']
 })
+
 export class HomeComponent {
 }
